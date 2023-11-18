2d544f0ee71dba0e24f3169e17c687af9086b03f
finnsatoshi03 <finnsatoshi03@gmail.com>
2023-11-18 15:43:26 +0800

    Add login buttons and account verification feature

--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -1,9 +1,10 @@
-import { Image, View, Text } from "react-native";
+import { Image, View, Text, Pressable } from "react-native";
 import { globalStyles } from "../styles/globalStyles";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import LoginButon from "./loginButtons";
 
 export default function LoginComponent() {
   return (
@@ -48,6 +49,45 @@ export default function LoginComponent() {
           power up your efficiency effortlessly!
         </Text>
       </View>
+      <View
+        style={{
+          width: wp("70%"),
+          position: "absolute",
+          zIndex: 3,
+          top: hp("55%"),
+          justifyContent: "center",
+          alignSelf: "center",
+        }}
+      >
+        <LoginButon text="Admin" />
+      </View>
+      <View
+        style={{
+          width: wp("70%"),
+          position: "absolute",
+          zIndex: 3,
+          top: hp("63%"),
+          justifyContent: "center",
+          alignSelf: "center",
+        }}
+      >
+        <LoginButon text="User" />
+      </View>
+      <View
+        style={{
+          position: "absolute",
+          zIndex: 3,
+          top: hp("70%"),
+          justifyContent: "center",
+          alignSelf: "center",
+        }}
+      >
+        <Pressable>
+          <Text style={{ fontFamily: "montserrat-regular" }}>
+            Account Verification
+          </Text>
+        </Pressable>
+      </View>
       <View style={{ position: "relative", top: hp("20%") }}>
         <Image
           style={{
@@ -71,6 +111,24 @@ export default function LoginComponent() {
             borderTopRightRadius: 100,
           }}
         ></View>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "flex-end",
+          alignSelf: "flex-end",
+          //   position: "absolute",
+          top: hp("10%"),
+          zIndex: 3,
+        }}
+      >
+        <Pressable>
+          <Text
+            style={{ fontFamily: "montserrat-light", fontSize: hp("1.2%") }}
+          >
+            Developers
+          </Text>
+        </Pressable>
       </View>
     </View>
   );
