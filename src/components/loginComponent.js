30e82f89ff7b22eea213cf03186063f8873948cb
finnsatoshi03 <finnsatoshi03@gmail.com>
2023-11-18 15:04:13 +0800

    Refactor loginComponent styles

--- src/components/loginComponent.js
+++ src/components/loginComponent.js
@@ -7,26 +7,43 @@ import {
 
 export default function LoginComponent() {
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <Image
-        style={{ height: hp("20%"), width: wp("40%") }}
+        style={{
+          position: "absolute",
+          top: hp("5%"),
+          zIndex: 3,
+          justifyContent: "center",
+          alignSelf: "center",
+          height: hp("30%"),
+          width: wp("60%"),
+        }}
         source={require("../../assets/logo.png")}
       />
-      <View
-        style={{
-          backgroundColor: "#94acaa",
-          height: hp("100%"),
-          width: wp("100%"),
-        }}
-      ></View>
-      <View
-        style={{
-          backgroundColor: "#f3fadc",
-          height: hp("100%"),
-          width: wp("100%"),
-          marginLeft: wp("-5.8%"),
-        }}
-      ></View>
+      <View style={{ position: "relative", top: hp("20%") }}>
+        <Image
+          style={{
+            position: "absolute",
+            zIndex: 2,
+            height: hp("100%"),
+            width: wp("100%"),
+            marginLeft: wp("-30.8%"),
+          }}
+          source={require("../../assets/background-overlay.png")}
+          resizeMode="stretch"
+        />
+        <View
+          style={{
+            position: "absolute",
+            zIndex: 1,
+            backgroundColor: "#f3fadc",
+            height: hp("100%"),
+            width: wp("100%"),
+            // marginLeft: wp("-5.8%"),
+            borderTopRightRadius: 100,
+          }}
+        ></View>
+      </View>
     </View>
   );
 }
