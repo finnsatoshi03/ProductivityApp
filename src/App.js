3b08e00800499d6aed6b195132f7ddd1a74e0800
finnsatoshi03 <finnsatoshi03@gmail.com>
2023-11-19 04:56:34 +0800

    Added chat card

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navigator from "./Layout/navigationStack";
 import Modal from "./components/modal.js";
 import { View, Text } from "react-native";
 import { globalStyles } from "./styles/globalStyles";
-import EventCard from "./components/eventCard";
+import ChatCard from "./components/chatCard";
 
 const getFonts = () =>
   Font.loadAsync({
@@ -32,12 +32,7 @@ export default function App() {
     return (
       <>
         <View style={globalStyles.container}>
-          <EventCard
-            date="22 Jul 2022"
-            time="1:00PM"
-            event="Tree sadsaiods sijadioasj doa sd"
-            location="Tree sadsaiods sijadioasj doa sd dkapso kdpsao kdpsao kdaps sadpkaos"
-          />
+          <ChatCard />
         </View>
         {/* <Navigator /> */}
       </>
---
+++ src/components/chatCard.js
@@ -0,0 +1,15 @@
+import { View, Text, Image } from "react-native";
+import { globalStyles } from "../styles/globalStyles";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+
+export default function chatCard() {
+  return (
+    <View>
+      <View></View>
+      <View></View>
+    </View>
+  );
+}
