69a24fba5546dd37145ad0c1a9003c3d1556eaf5
“jeeybii” <jrrybytjd23@gmail.com>
2023-11-21 03:41:14 +0800

    Report view

--- src/components/imagefetchAPI.js
+++ src/components/imagefetchAPI.js
@@ -2,7 +2,14 @@ const UNSPLASH_ACCESS_KEY = 'DwofUXPr1rcqUsoyYR2_Y9nAYxj8Z7z3rCxvpRrF7gc';
 
 export default async function fetchSquareImage(query) {
   try {
-    const apiUrl = 'https://api.unsplash.com/photos/random?client_id=${UNSPLASH_ACCESS_KEY}&query="${encodeURIComponent(query)}"';
+    const params = new URLSearchParams({
+        client_id: UNSPLASH_ACCESS_KEY,
+        query: encodeURIComponent(query),
+        orientation: 'landscape', // Example additional parameter for landscape images
+        // Add more parameters as needed, such as color, collections, etc.
+      });
+  
+      const apiUrl = `https://api.unsplash.com/photos/random?${params}`;
     
     const response = await fetch(apiUrl);
     const data = await response.json();
