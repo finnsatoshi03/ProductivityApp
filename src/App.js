93feacf9f3ac4973b08ca90c9a8274a72aa9cc7a
finnsatoshi03 <finnsatoshi03@gmail.com>
2023-11-21 12:59:01 +0800

    Added Chats screen and refactor searchbar component

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ export default function App() {
       <>
         <View style={globalStyles.container}>
           <Chats />
-          <Searchbar />
+          {/* <Searchbar /> */}
           {/* <Conversation /> */}
         </View>
         {/*<Sidebar />*/}
--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,7 +1,10 @@
-import { Text, View, Image } from "react-native";
+import React from "react";
+import { TextInput, View, Image, Pressable } from "react-native";
 import { globalStyles } from "./../styles/globalStyles";
 
 export default function Searchbar({ placeholder, bgColor, padding }) {
+  const inputRef = React.useRef();
+
   return (
     <View style={{ flex: 1 }}>
       <View
@@ -10,18 +13,23 @@ export default function Searchbar({ placeholder, bgColor, padding }) {
           alignItems: "center",
           gap: 15,
           backgroundColor: bgColor ? bgColor : globalStyles.colors.green200,
-          paddingVertical: padding ? padding : 10,
-          paddingHorizontal: padding ? padding : 20,
+          paddingVertical: padding ? 10 : undefined,
+          paddingHorizontal: padding ? 20 : undefined,
           borderRadius: 20,
         }}
       >
-        <Image
-          style={{ height: 20, width: 20 }}
-          source={require("./../../assets/search.png")}
+        <Pressable onPress={() => inputRef.current.focus()}>
+          <Image
+            style={{ height: 20, width: 20 }}
+            source={require("./../../assets/search.png")}
+          />
+        </Pressable>
+        <TextInput
+          ref={inputRef}
+          style={{ fontFamily: globalStyles.fontStyle.regular }}
+          placeholder={placeholder || "Search"}
+          width={"100%"}
         />
-        <Text style={{ fontFamily: globalStyles.fontStyle.regular }}>
-          {placeholder || "Search"}
-        </Text>
       </View>
     </View>
   );
