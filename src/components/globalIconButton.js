a97fa27be65231cac294a056406fd81f5fc115f3
finnsatoshi03 <finnsatoshi03@gmail.com>
2023-11-27 20:10:49 +0800

    Update globalIconButton and viewEvent components

--- src/components/globalIconButton.js
+++ src/components/globalIconButton.js
@@ -1,31 +1,39 @@
-import React from 'react';
-import { TouchableOpacity, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React from "react";
+import { TouchableOpacity, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5"; // Change this line
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
+} from "react-native-responsive-screen";
 
-const buttonSize = wp('12%'); // Adjust the percentage as needed
+const buttonSize = wp("12%"); // Adjust the percentage as needed
 
-const GlobalIconButton = ({ iconType, onPressed, color = '#000' }) => {
+const GlobalIconButton = ({ iconType, onPressed, color = "#000" }) => {
   let iconName;
+  let iconStyle = "solid"; // Default to solid
 
   switch (iconType) {
-    case 'bar':
-      iconName = 'bars';
+    case "bar":
+      iconName = "bars";
       break;
-    case 'search':
-      iconName = 'search';
+    case "search":
+      iconName = "search";
       break;
-    case 'trash':
-      iconName = 'trash';
+    case "trash":
+      iconName = "trash";
       break;
-    case 'plus':
-      iconName = 'plus';
+    case "plus":
+      iconName = "plus";
+      break;
+    case "solid-star":
+      iconName = "star";
+      break;
+    case "outlined-star":
+      iconName = "star";
+      iconStyle = "regular";
       break;
     default:
-      iconName = 'bars';
+      iconName = "bars";
   }
 
   return (
@@ -36,17 +44,22 @@ const GlobalIconButton = ({ iconType, onPressed, color = '#000' }) => {
       ]}
       onPress={onPressed}
     >
-      <Icon name={iconName} size={buttonSize * 0.5} color={color} />
+      <Icon
+        name={iconName}
+        size={buttonSize * 0.5}
+        color={color}
+        solid={iconStyle === "solid"}
+      />
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: buttonSize * 0.5,
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
     borderWidth: 1,
   },
 });
