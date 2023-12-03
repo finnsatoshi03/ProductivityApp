ec9312239d51f86d17ce878dcad87ce8427ddd53
finnsatoshi03 <finnsatoshi03@gmail.com>
2023-12-03 17:28:43 +0800

    Add date-fns library and implement holiday checking

--- src/screens/calendar.js
+++ src/screens/calendar.js
@@ -10,6 +10,7 @@ import {
   StatusBar,
 } from "react-native";
 import moment from "moment";
+import { isSameDay } from "date-fns";
 import { globalStyles } from "./../styles/globalStyles";
 import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 import Header from "./../components/header";
@@ -26,17 +27,17 @@ import { useData } from "./../DataContext";
 
 import axios from "axios";
 import "../../global";
-import { format } from 'date-fns-tz';
-import * as Notifications from 'expo-notifications';
+import { format } from "date-fns-tz";
+import * as Notifications from "expo-notifications";
 
 export default function Calendar({ navigation, route }) {
   const { fullname, user, user_id, role } = route.params;
-  
+
   const [isSidebarVisible, setSidebarVisible] = useState(false);
   const { eventData, setEventData } = useData();
   const [loading, setLoading] = useState(true);
   const [filteredEvents, setFilteredEvents] = useState([]);
-  const [selectedDay, setSelectedDay] = useState(moment().format('YYYY-MM-DD'));
+  const [selectedDay, setSelectedDay] = useState(moment().format("YYYY-MM-DD"));
 
   const deleteEvent = (eventTitleToDelete) => {
     if (eventData.length != 0) {
@@ -47,17 +48,75 @@ export default function Calendar({ navigation, route }) {
     console.log(`Event ${eventTitleToDelete} has been deleted.`);
   };
 
+  const holidays = {
+    "2023-01-01": "New Year’s Day",
+    "2023-04-06": "Maundy Thursday",
+    "2023-04-07": "Good Friday",
+    "2023-04-10": "Day of Valor (Araw ng Kagitingan)",
+    "2023-04-21": "Eid'l Fitr (Feast of Ramadhan)",
+    "2023-05-01": "Labor Day",
+    "2023-06-12": "Independence Day",
+    "2023-06-28": "Eid'l Adha (Feast of Sacrifice)",
+    "2023-08-28": "National Heroes Day",
+    "2023-11-27": "Bonifacio Day",
+    "2023-12-25": "Christmas Day",
+    "2023-12-30": "Rizal Day",
+    "2023-02-24": "EDSA People Power Revolution Anniversary",
+    "2023-04-08": "Black Saturday",
+    "2023-08-21": "Ninoy Aquino Day",
+    "2023-11-01": "All Saints’ Day",
+    "2023-12-08": "Feast of the Immaculate Conception of Mary",
+    "2023-12-31": "Last Day of the Year",
+    "2023-10-30": "(Barangay and SK Elections)",
+    "2023-11-02": "(Thursday)",
+    "2023-01-02": "(Monday)",
+    "2023-11-01":
+      "All Saints' Day & All Souls' Day (Take a leave on November 3)",
+    "2023-11-25": "Bonifacio Day",
+    "2023-12-08": "Feast of the Immaculate Concepcion",
+    "2023-12-23": "Christmas Day",
+    "2023-12-30":
+      "Rizal Day, Last Day of the Year, and New Year's Day (Take a leave on November 3)",
+    "2024-01-01": "New Year’s Day",
+    "2024-03-28": "Maundy Thursday",
+    "2024-03-29": "Good Friday",
+    "2024-04-09": "Day of Valor (Araw ng Kagitingan)",
+    "2024-05-01": "Labor Day",
+    "2024-06-12": "Independence Day",
+    "2024-08-26": "National Heroes Day",
+    "2024-11-30": "Bonifacio Day",
+    "2024-12-25": "Christmas Day",
+    "2024-12-30": "Rizal Day",
+    "2024-02-10": "Chinese New Year",
+    "2024-03-30": "Black Saturday",
+    "2024-08-21": "Ninoy Aquino Day",
+    "2024-11-01": "All Saints’ Day",
+    "2024-11-02": "All Souls’ Day",
+    "2024-12-08": "Feast of the Immaculate Conception of Mary",
+    "2024-12-24": "Christmas Eve",
+    "2024-12-31": "Last Day of the Year",
+  };
+
+  const isHoliday = (date) => {
+    return Object.keys(holidays).some((holiday) =>
+      isSameDay(new Date(date), new Date(holiday))
+    );
+  };
+
   const handleDayPress = (selectedDate) => {
     setSelectedDay(selectedDate);
-    const localSelectedDate = format(new Date(selectedDate), 'yyyy-MM-dd', { timeZone: 'Asia/Manila' });
+    const localSelectedDate = format(new Date(selectedDate), "yyyy-MM-dd", {
+      timeZone: "Asia/Manila",
+    });
     if (eventData.length != 0) {
       const filtered = eventData.filter(
-        (event) => format(new Date(event.datetime), 'yyyy-MM-dd', { timeZone: 'Asia/Manila' }) === localSelectedDate
+        (event) =>
+          format(new Date(event.datetime), "yyyy-MM-dd", {
+            timeZone: "Asia/Manila",
+          }) === localSelectedDate
       );
       setFilteredEvents(filtered);
     }
-   
-    
   };
 
   const formatDate = (date) => {
@@ -92,11 +151,15 @@ export default function Calendar({ navigation, route }) {
   }, []);
 
   useEffect(() => {
-
     if (selectedDay !== "" && Array.isArray(eventData)) {
-      const localSelectedDate = format(new Date(selectedDay), 'yyyy-MM-dd', { timeZone: 'Asia/Manila' });
+      const localSelectedDate = format(new Date(selectedDay), "yyyy-MM-dd", {
+        timeZone: "Asia/Manila",
+      });
       const filtered = eventData.filter(
-        (event) => format(new Date(event.datetime), 'yyyy-MM-dd', { timeZone: 'Asia/Manila' }) === localSelectedDate
+        (event) =>
+          format(new Date(event.datetime), "yyyy-MM-dd", {
+            timeZone: "Asia/Manila",
+          }) === localSelectedDate
       );
       setFilteredEvents(filtered);
     }
@@ -141,55 +204,55 @@ export default function Calendar({ navigation, route }) {
       setLoading(false);
     }
   };
-  
+
   const requestNotificationPermission = async () => {
     const { status } = await Notifications.requestPermissionsAsync();
-    if (status === 'granted') {
-      console.log('Notification permission granted.');
+    if (status === "granted") {
+      console.log("Notification permission granted.");
     } else {
-      console.log('Notification permission denied.');
+      console.log("Notification permission denied.");
     }
   };
 
   Notifications.addNotificationReceivedListener((notification) => {
-    console.log('Notification received:', notification);
+    console.log("Notification received:", notification);
   });
-  
+
   Notifications.addNotificationResponseReceivedListener((response) => {
-    console.log('Notification response received:', response);
+    console.log("Notification response received:", response);
   });
 
   const scheduleNotificationOneHourBeforeEvent = async (eventTime) => {
     const currentTime = new Date(); // Current date/time
-  
+
     // Calculate the time difference in milliseconds between current time and event time
     const timeDiff = eventTime.getTime() - currentTime.getTime();
-  
+
     // Calculate one hour in milliseconds
     const oneHourInMilliseconds = 60 * 60 * 1000;
-  
+
     // Calculate the trigger time by subtracting one hour from the event time
     const triggerTime = new Date(eventTime.getTime() - oneHourInMilliseconds);
-  
+
     // Check if the event is at least one hour from the current time
     if (timeDiff > oneHourInMilliseconds) {
       // Schedule the notification one hour before the event
       await Notifications.scheduleNotificationAsync({
         content: {
-          title: 'Event Reminder!',
-          body: 'Your event is starting in one hour!',
+          title: "Event Reminder!",
+          body: "Your event is starting in one hour!",
         },
         trigger: {
           date: triggerTime,
         },
       });
-      console.log('Notification was scheduled')
+      console.log("Notification was scheduled");
     } else {
-      console.log('Event is less than one hour away. Cannot schedule notification.');
+      console.log(
+        "Event is less than one hour away. Cannot schedule notification."
+      );
     }
   };
-  
-  
 
   return (
     <>
@@ -257,7 +320,9 @@ export default function Calendar({ navigation, route }) {
                 <Text
                   style={{ textAlign: "center", fontSize: 18, color: "gray" }}
                 >
-                  Embracing Tranquility 🎉 {"\n"} No Current Events Today!
+                  {isHoliday(selectedDay)
+                    ? `Celebrate ${holidays[selectedDay]}! 🎉`
+                    : "Embracing Tranquility 🎉 \n No Current Events Today!"}
                 </Text>
               </View>
             ) : (
