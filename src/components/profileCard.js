bdd0b83ee52c61c874cc33f7c6260cee28cc73bf
finnsatoshi03 <finnsatoshi03@gmail.com>
2023-12-22 01:30:50 +0800

    Add axios import and fetch user image in profileCard component

--- src/components/profileCard.js
+++ src/components/profileCard.js
@@ -11,6 +11,7 @@ import { globalStyles } from "../styles/globalStyles";
 import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 import Avatar from "./avatar";
 import LottieView from "lottie-react-native";
+import axios from "axios";
 
 export default function profileCard({
   avatar,
@@ -23,7 +24,7 @@ export default function profileCard({
   selectAll,
   verify,
   participants,
-  purpose
+  purpose,
 }) {
   const progress = useRef(new Animated.Value(0)).current;
   const [clickCount, setClickCount] = useState(0);
@@ -35,35 +36,33 @@ export default function profileCard({
       useNativeDriver: true,
     }).start();
   };
-  
+
   const nameParts = fullname.split(" ");
 
   const isIdFound = () => {
-    if (purpose !== 'view') {
+    if (purpose !== "view") {
       return participants.some((participant) => participant.id === id);
-    } else {      
-      return false; 
+    } else {
+      return false;
     }
   };
- 
-  
-  const handlePress = () => {    
+
+  const handlePress = () => {
     if (selectAll) {
       Animated.timing(progress, {
         toValue: 0,
         duration: 500,
         useNativeDriver: true,
       }).start();
-    } 
-  
+    }
+
     const isParticipantAdded = participants.some(
       (participant) => participant.id === id
-      
     );
- 
+
     if (!isParticipantAdded) {
       setClickCount((prevCount) => prevCount + 1);
-  
+
       let endValue;
       switch (clickCount % 3) {
         case 0:
@@ -76,42 +75,68 @@ export default function profileCard({
           endValue = progress._value;
           break;
       }
-  
+
       Animated.timing(progress, {
         toValue: endValue,
         duration: 500,
         useNativeDriver: true,
       }).start();
-      onParticipantSelect({ avatar, fullname, id },id);
+      onParticipantSelect({ avatar, fullname, id }, id);
     } else {
       Animated.timing(progress, {
         toValue: 0,
         duration: 500,
         useNativeDriver: true,
       }).start();
-      onParticipantSelect({ avatar, fullname, id },id);
+      onParticipantSelect({ avatar, fullname, id }, id);
     }
   };
-    
+
   useEffect(() => {
     // Update the progress value based on the selectAll state
     Animated.timing(progress, {
       toValue: selectAll ? 0.5 : 0,
       duration: 500,
       useNativeDriver: true,
-    }).start();   
+    }).start();
   }, [selectAll]);
 
   useEffect(() => {
-    // Run the animation if the ID is found       
-    if (purpose === 'edit') {      
+    // Run the animation if the ID is found
+    if (purpose === "edit") {
       if (isIdFound()) {
-        console.log(purpose); 
+        console.log(purpose);
         runAnimation();
       }
-      
     }
   }, []);
+
+  const [userImage, setUserImage] = useState(null);
+
+  const fetchUserImage = async () => {
+    let baseurlimage = `${global.baseurl}:4000/userImage/${id}`;
+
+    const responseimage = await axios.get(baseurlimage);
+
+    // Extract the image name from the response and build the correct URL
+    let imageName = responseimage.data.image.replace("\\", "/"); // Replace backslash with forward slash
+    imageName = imageName.replace("uploadsx", ""); // Remove 'uploadsx' from the image name
+    const originalNumber = imageName.split("/")[1].split("-")[0]; // Extract the number from the image name
+    let imageUrl = null;
+    if (originalNumber !== "null") {
+      const newNumber = "170" + originalNumber; // Prepend '170' to the original number
+      imageUrl = `${global.baseurl}:4000/uploads/${newNumber}-photo.jpeg`;
+    }
+
+    setUserImage(imageUrl);
+  };
+  console.log(userImage);
+  console.log(id);
+
+  useEffect(() => {
+    fetchUserImage();
+  }, []);
+
   return (
     <View
       style={{
@@ -125,7 +150,13 @@ export default function profileCard({
       }}
     >
       <View style={{ flexDirection: "row", alignItems: "center", gap: 30 }}>
-        <Avatar avatar={avatar} firstName={fullname} />
+        <Avatar
+          avatar={userImage || avatar}
+          firstName={fullname}
+          customHeight={hp("6.5%")}
+          customWidth={hp("6.5%")}
+          size={6}
+        />
         <View>
           {date ? (
             <Text
@@ -182,7 +213,7 @@ export default function profileCard({
           source={require("./../../assets/view.png")}
           style={{ width: hp("2.5%"), height: hp("2.5%") }}
         />
-      ) : (purpose !== 'view' ? (
+      ) : purpose !== "view" ? (
         <TouchableOpacity onPress={handlePress}>
           <LottieView
             progress={progress}
@@ -191,7 +222,7 @@ export default function profileCard({
             style={{ width: 50, height: 50 }}
           />
         </TouchableOpacity>
-      ) : null)}
+      ) : null}
     </View>
   );
 }
